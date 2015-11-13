d76911329ea7a9e6d07c021359f9fc01030e9aa6
JayaJHU <jayavijayen@hotmail.com>
2015-11-12 20:41:48 -0800

    Create practice.py

---
+++ practice.py
@@ -0,0 +1,43 @@
+def add(num1,num2):
+	return num1 + num2
+# print add (4,5)
+
+def subtract(num1,num2):
+ 	return num1 - num2
+# print subtract(9,5)
+
+def multiply(num1,num2):
+ 	return num1 * num2
+# print multiply(6,4)
+
+
+def multiply(num1,num2):
+ 	return num1 / num2
+# print multiply(35,5)
+
+
+def modulo(num1,num2):
+ 	return num1 % num2
+# print modulo(7,3)
+
+def power(num1,num2):
+	return num1 ** num2
+# # print power(3,3)
+
+
+# def square(num):
+#  	return num ** 2
+# print square(2)
+
+def square(num):
+ 	return power(num,2)
+# print square(2)
+
+
+# import math
+
+# def square(num):
+#  	return math.pow (num,2)
+# print square(5)
+
+def add(4,5) - def subtract(9,6)
